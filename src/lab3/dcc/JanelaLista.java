7a1075be25b1561f8f5dde617232eb7c339cefb6
11910875651 <11910875651@lab06m33.nrc.ice.ufjf.br>
2016-09-22 22:25:39 -0300

    Lista - Meio

---
+++ src/lab3/dcc/JanelaLista.java
@@ -0,0 +1,44 @@
+
+package lab3.dcc;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+class JanelaLista extends JFrame{
+    
+    private final JLabel lblNumero = new JLabel("Numero");
+    private final JTextField txtNumero = new JTextField(10);
+    private final JButton btnNumero = new JButton("+");
+    private final JList<Integer> lstNumeros = new JList<>(new DefaultListModel<Integer>());
+
+    public JanelaLista() {
+        super("Lista");
+        JPanel pnlEntrada = new JPanel(new BorderLayout(5, 5));
+        pnlEntrada.add(lblNumero, BorderLayout.WEST);
+        pnlEntrada.add(txtNumero, BorderLayout.CENTER);
+        pnlEntrada.add(btnNumero, BorderLayout.EAST);
+        
+        add(pnlEntrada, BorderLayout.NORTH);
+        add(lstNumeros, BorderLayout.CENTER);
+        
+        btnNumero.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DefaultListModel<Integer> modelo = (DefaultListModel<Integer>)lstNumeros.getModel();
+                modelo.addElement(Integer.parseInt(txtNumero.getText()));
+            }
+        });
+    }
+    
+    
+}
---
+++ src/lab3/dcc/aula2209dois.java
@@ -0,0 +1,20 @@
+
+package lab3.dcc;
+
+import javax.swing.JFrame;
+
+
+public class aula2209dois {
+
+
+    public static void main(String[] args) {
+        System.out.println("Comecou");
+        JanelaLista j = new JanelaLista();
+        j.setSize(400,300);
+        j.setLocationRelativeTo(null);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setVisible(true);
+        System.out.println("fechando");
+    }
+    
+}
